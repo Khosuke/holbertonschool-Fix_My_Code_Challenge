0f755df29e3cca7db075a0b8d6da6f5dfb8fc7b9
Khosuke <9814@holbertonstudents.com>
2025-05-12 13:56:50 +0200

    Change to take only 1 argument, doesn't work with 0 or more than 1 argument

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -7,16 +7,14 @@
 */
 
 
-if (process.argv.length <= 2) {
+if (process.argv.length != 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
 
-// Removed the 2nd argument of parseInt that 
-// converted the first argument in base 16
-size = parseInt(process.argv[2])
+size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
